6cf886d210714db088b1c6ef846adc2e57292764
Ilia Vakhitov <iv7180@gmail.com>
2020-01-31 21:00:36 +1000

    Solved Merge Two Sorted Lists

--- mergelists.py
+++ mergelists.py
@@ -18,20 +18,58 @@ class Solution(object):
         node_l11 = ListNode(1)
         node_l11.next = node_l12
 
-        node_l24 = ListNode(4)        
+        node_l25 = ListNode(5)
+        node_l24 = ListNode(4)
+        node_l24.next = node_l25
         node_l23 = ListNode(3)
         node_l23.next = node_l24
         node_l21 = ListNode(1)
         node_l21.next = node_l23
 
         print(self.mergeTwoLists(node_l11, node_l21))
-        
-        
+        print(self.mergeTwoLists(node_l25, node_l14))
+
     def mergeTwoLists(self, l1, l2):
         """
         :type l1: ListNode
         :type l2: ListNode
         :rtype: ListNode
         """
+        res_list = []
+        c1 = l1
+        c2 = l2
+        while True:
+            if c1 is None and c2 is None:
+                break
+            if c1 is None:
+                res_list.append(ListNode(c2.val))
+                c2 = c2.next
+            elif c2 is None:
+                res_list.append(ListNode(c1.val))
+                c1 = c1.next
+            elif c2.val < c1.val:
+                res_list.append(ListNode(c2.val))
+                c2 = c2.next
+            elif c1.val < c2.val:
+                res_list.append(ListNode(c1.val))
+                c1 = c1.next
+            elif c1.val == c2.val:
+                res_list.append(ListNode(c1.val))
+                res_list.append(ListNode(c2.val))
+                c1 = c1.next
+                c2 = c2.next
 
+        # print(res_list)
+        if len(res_list) == 0:
+            return None
+        last = res_list.pop()
+        if len(res_list) == 0:
+            return last
+
+        while res_list:
+            curr = res_list.pop()
+            if curr is not None:
+                curr.next = last
+            last = curr
+        return curr
 
