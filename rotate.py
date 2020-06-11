b899e7f2bec206a4232c8eba39be4652f3719e2e
Ilia Vakhitov <iv7180@gmail.com>
2020-06-11 19:34:37 +1000

    # Rotate Array

---
+++ rotate.py
@@ -0,0 +1,87 @@
+# Rotate Array
+from typing import List
+
+class Solution:
+
+    def run(self):        
+        for k in range(15):
+            nums = [1,2,3,4,5,6,7]
+            #self.rotateCycle(nums, k)
+            print(self.rotateCopy(nums, k) == self.rotateCycle(nums, k))
+
+        #nums = ['1','2','3','4','5','6','7']
+        #self.rotateCycle(nums,2)
+
+    def rotateCopy(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        ln = len(nums)
+        if ln == 1:
+            return
+        if k > ln:
+            k =  k % ln
+        if k == 0:
+            return
+        arr = nums.copy()
+        for i in range(ln):
+            if i+k < ln:
+                nums[i+k] = arr[i]
+            else:
+                nums[i+k-ln] = arr[i]     
+            
+        return nums
+
+    def rotatePop(self, nums: List[int], k: int) -> None:
+        ln = len(nums)
+        if ln == 1:
+            return
+        if k > ln:
+            k =  k % ln
+        if k == 0:
+            return
+        left = False
+        if k > ln / 2:
+            k = ln - k
+            left = True
+        for i in range(k):
+            if left:
+                nums.append(nums.pop(0))
+            else:
+                nums.insert(0, nums.pop())
+        #print(nums)
+            
+        return nums
+
+    def rotateCycle(self, nums: List[int], k: int) -> None:
+        ln = len(nums)
+        if ln == 1:
+            return
+        if k > ln:
+            k =  k % ln
+        if k == 0:
+            return
+        changed = 0
+        i = 0        
+        while changed < ln:            
+            ni = i      
+            t = nums[i]
+            # Do while j != i and changed < ln
+            while changed < ln:                  
+                j = ni+k if ni+k < ln else ni+k-ln                                
+                pt = nums[j]
+                nums[j] = t
+                t = pt 
+                ni = j                
+                changed += 1
+                if j == i or changed >= ln:
+                    break
+            i += 1
+            if i >= ln:
+                break
+        #print(nums)
+            
+        return nums
+
+if __name__ == '__main__':
+    Solution().run()
