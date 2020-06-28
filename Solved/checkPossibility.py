df222e6752dbc293f16cc7cb1fcd48c6d53731fb
Ilia Vakhitov <iv7180@gmail.com>
2020-06-28 18:52:00 +1000

    Non-decreasing Array

--- Solved/checkPossibility.py
+++ Solved/checkPossibility.py
@@ -1,4 +1,5 @@
 # Non-decreasing Array
+
 from typing import List
 
 class Solution:
@@ -32,26 +33,42 @@ class Solution:
         nums = [4,4,3,3]
         print(f'{self.checkPossibility(nums)} - expected False')
 
-    def checkPossibility(self, nums: List[int]) -> bool:
-        length = len(nums)
-        if length == 0 or length == 1:
+        nums = [4,1,3,3]
+        print(f'{self.checkPossibility(nums)} - expected True')
+
+        nums = [4,4,1,3,3]
+        print(f'{self.checkPossibility(nums)} - expected False')
+
+        nums = [4,1,1,5,5]
+        print(f'{self.checkPossibility(nums)} - expected True')
+
+        nums = [4,4,1,1,5,5]
+        print(f'{self.checkPossibility(nums)} - expected False')
+
+    def isNonDecreasing(self, nums: List[int]) -> bool:
+        ln = len(nums)
+        if ln <= 1:
             return True
-        max_el = -1000000
-        has_el = False
-        for i in range(0, length-1):
+        
+        for i in range(0, ln-1):
             if nums[i] > nums[i+1]:
-                # second time next is lesser
-                if has_el:
-                    return False
-                # couldn't replace one
-                if nums[i] > max_el and nums[i+1] < max_el:
-                    return False
-                
-                has_el = True
-            
-            if max_el < nums[i]:
-               max_el = nums[i]            
-            
+                return False
+
+        return True
+
+    def checkPossibility(self, nums: List[int]) -> bool:
+        # Brute
+        # Delete problem element.
+        # Check if is non decreasing
+        ln = len(nums)
+        if ln <= 1:
+            return True
+        
+        for i in range(1, ln):
+            if nums[i-1] > nums[i]:
+                return self.isNonDecreasing(nums[:i]+nums[i+1:])\
+                    or self.isNonDecreasing(nums[:i-1]+nums[i:])
+
         return True
         
 
