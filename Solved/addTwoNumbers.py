39bb6896d907f41eeac14adfcabcd18877d9c2b6
iv7180 <iv7180@gmail.com>
2020-07-03 19:50:42 +1000

    Add Two Numbers

--- Solved/addTwoNumbers.py
+++ Solved/addTwoNumbers.py
@@ -19,41 +19,36 @@ class Solution:
 
     def run(self):
         #node_5 = ListNode(5)
-        node_4 = ListNode(4)
+        node_4 = ListNode(8)
         node_3 = ListNode(3)
-        node_2 = ListNode(2)
-        node_1 = ListNode(1)
+        node_2 = ListNode(9)
+        node_1 = ListNode(5)
         node_1.next = node_2
         node_2.next = node_3
         node_3.next = node_4
         print(self.addTwoNumbers(node_1, node_1))
-
-    def listToInt(self, node: ListNode) -> int:
-        k = 0
-        res = 0
-        while node:
-            res += node.val * 10**k
-            node = node.next
-            k += 1
-        return res
-    
-    def intToList(self, number: int) -> ListNode:
-        # Reverse string
-        # Make a list        
-        string = str(number)[::-1]
-        res = ListNode()
-        node = ListNode(int(string[0]))
-        res.next = node
-        for i in range(1,len(string)):
-            node.next = ListNode(int(string[i])) 
-            node = node.next
-        return res.next
     
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        return self.intToList(
-            self.listToInt(l1) + self.listToInt(l2)
-        )
-                       
+        # TODO
+        head = ListNode(0)
+        add = 0
+        node = head
+        while l1 or l2:
+            n1 = 0 if not l1 else l1.val
+            n2 = 0 if not l2 else l2.val
+            val = add + n1 + n2
+            add = val // 10
+            val = val % 10 
+            node.next = ListNode(val)
+            node = node.next
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+        if add:
+            node.next = ListNode(add)
+
+        return head.next                      
 
 if __name__ == '__main__':
     Solution().run()
